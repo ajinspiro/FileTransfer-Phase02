f3e4c2bd6016096ed4e77ec4f39deb37ccb77829
Arunkumar Anil Jolly <arunkumar.aniljolly@hrblock.com>
2025-03-25 18:24:30 +0530

    Add project files.

---
+++ Common/ClientOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Common
+{
+    public class ClientOptions(IPAddress ipAddress, Port port)
+    {
+        private readonly IPAddress _ipAddress = ipAddress;
+        private readonly Port _port = port;
+
+        public IPAddress IPAddress => _ipAddress;
+        public Port Port => _port;
+    }
+}
---
+++ Common/ClientV1.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Common
+{
+    public class ClientV1 : IClient
+    {
+        public async Task Run(ClientOptions options)
+        {
+            IPEndPoint ipEndPoint = new(options.IPAddress, options.Port);
+            using TcpClient tcpClient = new(ipEndPoint);
+        }
+    }
+}
