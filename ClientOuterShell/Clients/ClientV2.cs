4518529b7cc2cffc30b4bc0b17dcea8abae18b8e
Arunkumar Anil Jolly <arunkumar.aniljolly@hrblock.com>
2025-03-27 06:36:01 +0530

    changes

---
+++ ClientOuterShell/Clients/ClientV2.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace ClientOuterShell.Clients;
+
+/*
+ * V2 - writing bytes manually without using .NET's writer class
+ */
+
+public class ClientV2 : IClient
+{
+    public async Task Send(ClientOptions options)
+    {
+        using TcpClient tcpClient = new(options.IPAddress.ToString(), options.Port);
+        using NetworkStream channel = tcpClient.GetStream();
+
+        using FileStream fileStream = new(options.FilePath, FileMode.Open, FileAccess.Read);
+        string fileName = Path.GetFileName(options.FilePath);
+        PayloadMetadata payloadMetadata = new() { FileName = fileName, FileSize = fileStream.Length };
+        string metadata = JsonSerializer.Serialize(payloadMetadata);
+
+        // Step-1 Write metadata
+
+        byte[] metadataBytes = Encoding.Unicode.GetBytes(metadata);
+        // Step-1.1 Write metadata length
+        byte[] metadataLength = BitConverter.GetBytes(metadataBytes.Length);
+        channel.Write(metadataLength, 0, metadataLength.Length);
+
+        // Step-1.2 Write metadata
+        var metadataBytesInChunks = metadataBytes.Chunk(64 * 1024);
+        foreach (var chunk in metadataBytesInChunks)
+        {
+            await channel.WriteAsync(chunk, 0, chunk.Length, CancellationToken.None);
+        }
+        await channel.FlushAsync();
+    }
+}
---
+++ ServerOuterShell/ClientProcessors/ClientProcessorV2.cs
@@ -0,0 +1,21 @@
+﻿using Common;
+using System.Net.Sockets;
+
+namespace ServerOuterShell.ClientProcessors;
+
+/*
+ * V2 - reading bytes manually without using .NET's reader class
+ */
+
+public class ClientProcessorV2 : IClientProcessor
+{
+    public async Task Receive(TcpClient clientConnection)
+    {
+        using NetworkStream channel = clientConnection.GetStream();
+        byte[] metadataLengthInBytes = new byte[sizeof(long)];
+        await channel.ReadAsync(metadataLengthInBytes, 0, metadataLengthInBytes.Length);
+        long metadataLength = BitConverter.ToInt64(metadataLengthInBytes);
+        Console.WriteLine(metadataLength);
+
+    }
+}
