9d01fad31500bbcedb04dc533231fdae8845c166
arun <ajarunkumar96@gmail.com>
2025-04-04 18:19:08 +0530

    changes

--- ClientOuterShell/Clients/ClientV3.cs
+++ ClientOuterShell/Clients/ClientV3.cs
@@ -2,7 +2,7 @@
 
 namespace ClientOuterShell.Clients
 {
-    // This client will connect to an HTTP server, send GET request, retrieve response and dumps it in console.
+    // This client will use raw socket to connect to an HTTP server, send GET request, retrieve response and dumps it in console.
     // This client will not have a server implementation. We will be using the wikipedia page of a keralite mathematician Nīlakaṇṭha Somayāji (https://en.wikipedia.org/wiki/Nilakantha_Somayaji) to get the response.
     public class ClientV3 : IClient
     {
