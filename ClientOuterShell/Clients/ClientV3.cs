498c482d150d90a9311cb1a8527a8f6a9bab66a2
arun <ajarunkumar96@gmail.com>
2025-04-04 18:17:57 +0530

    V3

---
+++ ClientOuterShell/Clients/ClientV3.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+namespace ClientOuterShell.Clients
+{
+    // This client will connect to an HTTP server, send GET request, retrieve response and dumps it in console.
+    // This client will not have a server implementation. We will be using the wikipedia page of a keralite mathematician Nīlakaṇṭha Somayāji (https://en.wikipedia.org/wiki/Nilakantha_Somayaji) to get the response.
+    public class ClientV3 : IClient
+    {
+        public Task Send(ClientOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
