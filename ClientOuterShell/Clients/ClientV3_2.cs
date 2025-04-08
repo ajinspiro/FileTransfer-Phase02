819490ec2ccc901d323bf4da75917e4cf8e7a8d9
arun <ajarunkumar96@gmail.com>
2025-04-08 22:40:03 +0530

    changes

--- ClientOuterShell/Clients/ClientV3_2.cs
+++ ClientOuterShell/Clients/ClientV3_2.cs
@@ -1,63 +1,16 @@
 ﻿using Common;
-using System.Collections.Generic;
 using System.Net.Sockets;
-using System.Reflection.PortableExecutable;
 using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace ClientOuterShell.Clients;
 
-/*
- * With this version, I'm kicking off an attempt to learn how the network stack works when
- * I send a HTTP(S) request from a process. The end goal is to connect to wikipedia using 
- * raw socket through TLS, fetch the HTML of the page of legendary keralite mathematician 
- * Nīlakaṇṭha Somayāji (at https://en.wikipedia.org/wiki/Nilakantha_Somayaji) and dump it 
- * in console. This is an ambitious dream for an application developer like me, hence I 
- * need to create smaller intermediate goals. No clients in version 3 will be having 
- * ClientProcessors since I will be using servers in internet.
- * */
-
-/* This version (3.1), will be the first step. I will be connecting to http://httpforever.com 
- * through a socket using STREAM and I chose this site as a first step because it doesn't 
- * implement TLS security. 
- * */
-
-/* Limitation/Bug: The buffer is hard coded to be 3KB. The uncompressed response is larger than 
- * 3KB. This means the response will be cut off in the middle.
- */
-public class ClientV3_1 : IClient
-{
-    public async Task Send(ClientOptions _)
-    {
-        Socket clientSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        // no need to bind the socket because its a client
-        await clientSocket.ConnectAsync("httpforever.com", 80);
-        string httpRequest =
-@"GET / HTTP/1.1
-Host: httpforever.com
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-Accept-Language: en-US,en;q=0.5
-
-";
-        byte[] httpRequestBytes = Encoding.ASCII.GetBytes(httpRequest);
-        int bytesSent = await clientSocket.SendAsync(httpRequestBytes);
-        if (bytesSent != httpRequestBytes.Length)
-        {
-            throw new Exception("Some data were not sent.");
-        }
-        byte[] response = new byte[3 * 1024];
-        int bytesRead = await clientSocket.ReceiveAsync(response);
-        string str = Encoding.ASCII.GetString(response);
-        Console.WriteLine(str);
-        await clientSocket.DisconnectAsync(false);
-    }
-}
-
-/* In this version I will retrieve the full response sent by the server. I will be doing it 
- * by reading the response in chunks of 64KB using a loop. If less than 64KB data was read,
- * it means it was the last chunk.
+/* In this version I will log the full response sent by the server to the console. To do this,
+ * first I need to parse the headers and find out the value of content-length header. This header
+ * tells us how long the response body is. Detecting the end of the header section is a little 
+ * bit challenging because headers and body are sent as a single stream of text. But the headers
+ * will be separated from the body by a CRLF (\r\n). This means occurance of double CRLF (\r\n\r\n)
+ * is the end of headers. Once the content-length value is known, we can read exactly that much to
+ * process the body in full.
  */
 public class ClientV3_2 : IClient
 {
@@ -85,8 +38,11 @@ Accept-Language: en-US,en;q=0.5
         Console.WriteLine();
         HeaderList headerList = await ParseHeaders(clientSocket);
         var contentLength = int.Parse(headerList["Content-Length"] ?? throw new Exception());
-        Console.WriteLine(contentLength);
-
+        byte[] buffer = new byte[contentLength];
+        await clientSocket.ReceiveAsync(buffer);
+        StringBuilder responseBodyBuilder = new();
+        responseBodyBuilder.Append(Encoding.ASCII.GetString(buffer));
+        Console.WriteLine(responseBodyBuilder.ToString());
         await clientSocket.DisconnectAsync(false);
     }
 
